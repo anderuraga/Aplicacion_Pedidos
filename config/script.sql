ab34ac5a81a0a7f77557e12fde6bfc0d3a7d0664
JavierGM2000 <javiergm2009@gmail.com>
2025-04-29 12:37:29 +0200

    Cambiada columna tipo de subconcepto de tiny int a enum

--- config/script.sql
+++ config/script.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 09, 2025 at 11:58 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-04-2025 a las 12:36:46
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `elorrieta`
+-- Base de datos: `elorrieta`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `areas_gastos`
+-- Estructura de tabla para la tabla `areas_gastos`
 --
 
 CREATE TABLE `areas_gastos` (
@@ -34,7 +34,7 @@ CREATE TABLE `areas_gastos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
 --
--- Dumping data for table `areas_gastos`
+-- Volcado de datos para la tabla `areas_gastos`
 --
 
 INSERT INTO `areas_gastos` (`id`, `id_departamento`, `nombre`) VALUES
@@ -42,12 +42,14 @@ INSERT INTO `areas_gastos` (`id`, `id_departamento`, `nombre`) VALUES
 (4, 3, 'Materiales de fabricación'),
 (5, 5, 'Elorrieta Prueba 23'),
 (7, 4, 'test 2 22'),
-(8, 5, 'Prueba nueva');
+(8, 5, 'Prueba nueva'),
+(9, 4, 'Super prueba'),
+(10, 3, 'Segunda area gasto');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `departamentos`
+-- Estructura de tabla para la tabla `departamentos`
 --
 
 CREATE TABLE `departamentos` (
@@ -56,20 +58,26 @@ CREATE TABLE `departamentos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
 --
--- Dumping data for table `departamentos`
+-- Volcado de datos para la tabla `departamentos`
 --
 
 INSERT INTO `departamentos` (`id`, `nombre`) VALUES
+(1, 'Borrado2r'),
 (2, 'Conserjería'),
+(9, 'cuarta prueba'),
 (5, 'Electricidad'),
 (3, 'Fabricación Mecánica'),
+(11, 'Materiales de fabricación'),
+(6, 'Nuevo departamento'),
 (4, 'Química'),
-(1, 'Secretaría');
+(10, 'quinta prueba'),
+(7, 'segunda prueba'),
+(8, 'Tercera prueba');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `estado`
+-- Estructura de tabla para la tabla `estado`
 --
 
 CREATE TABLE `estado` (
@@ -77,10 +85,19 @@ CREATE TABLE `estado` (
   `nombre` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `estado`
+--
+
+INSERT INTO `estado` (`id`, `nombre`) VALUES
+(1, 'Borrador'),
+(2, 'Enviado Secretaría'),
+(3, 'Enviado Proveedor');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `facturas`
+-- Estructura de tabla para la tabla `facturas`
 --
 
 CREATE TABLE `facturas` (
@@ -97,7 +114,7 @@ CREATE TABLE `facturas` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `incidencias`
+-- Estructura de tabla para la tabla `incidencias`
 --
 
 CREATE TABLE `incidencias` (
@@ -110,7 +127,7 @@ CREATE TABLE `incidencias` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `materiales`
+-- Estructura de tabla para la tabla `materiales`
 --
 
 CREATE TABLE `materiales` (
@@ -121,7 +138,7 @@ CREATE TABLE `materiales` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `movimientos`
+-- Estructura de tabla para la tabla `movimientos`
 --
 
 CREATE TABLE `movimientos` (
@@ -135,7 +152,7 @@ CREATE TABLE `movimientos` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pedidos`
+-- Estructura de tabla para la tabla `pedidos`
 --
 
 CREATE TABLE `pedidos` (
@@ -157,7 +174,7 @@ CREATE TABLE `pedidos` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pedidos_estados`
+-- Estructura de tabla para la tabla `pedidos_estados`
 --
 
 CREATE TABLE `pedidos_estados` (
@@ -171,7 +188,7 @@ CREATE TABLE `pedidos_estados` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `presupuestos`
+-- Estructura de tabla para la tabla `presupuestos`
 --
 
 CREATE TABLE `presupuestos` (
@@ -185,7 +202,7 @@ CREATE TABLE `presupuestos` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `proveedores`
+-- Estructura de tabla para la tabla `proveedores`
 --
 
 CREATE TABLE `proveedores` (
@@ -208,19 +225,27 @@ CREATE TABLE `proveedores` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `subconceptos`
+-- Estructura de tabla para la tabla `subconceptos`
 --
 
 CREATE TABLE `subconceptos` (
   `id` int(11) NOT NULL,
   `nombre` varchar(45) NOT NULL,
-  `tipo` tinyint(1) NOT NULL
+  `tipo` enum('Fungible','Inventariable') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `subconceptos`
+--
+
+INSERT INTO `subconceptos` (`id`, `nombre`, `tipo`) VALUES
+(1, 'Subconcepto 1', 'Inventariable'),
+(2, 'Subconcepto 2', 'Fungible');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tipos_servicio`
+-- Estructura de tabla para la tabla `tipos_servicio`
 --
 
 CREATE TABLE `tipos_servicio` (
@@ -228,10 +253,18 @@ CREATE TABLE `tipos_servicio` (
   `nombre` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `tipos_servicio`
+--
+
+INSERT INTO `tipos_servicio` (`id`, `nombre`) VALUES
+(1, 'Materiales de fabricación'),
+(2, 'Equipos Informáticos');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transacciones`
+-- Estructura de tabla para la tabla `transacciones`
 --
 
 CREATE TABLE `transacciones` (
@@ -243,23 +276,25 @@ CREATE TABLE `transacciones` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
 --
--- Dumping data for table `transacciones`
+-- Volcado de datos para la tabla `transacciones`
 --
 
 INSERT INTO `transacciones` (`id`, `id_area`, `fecha`, `descripcion`, `cantidad`) VALUES
 (1, 4, '2025-04-01 12:00:00', 'Ingreso para área materiales', 10001.5),
 (2, 1, '2025-04-02 14:00:00', 'Ingreso para gastos generales', 9000.02),
-(3, 5, '2025-04-02 12:00:00', 'Nuevo ingreos', 10000);
+(3, 5, '2025-04-02 12:00:00', 'Nuevo ingreos', 10000),
+(4, 4, '2025-04-29 12:00:00', 'Nuevo ingreos prueba', 599.99),
+(5, 1, '2025-04-29 11:00:00', 'Otra prueba', 200.5);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuarios`
+-- Estructura de tabla para la tabla `usuarios`
 --
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
-  `tipo` int(11) NOT NULL,
+  `tipo` int(1) NOT NULL DEFAULT 0,
   `nombre` varchar(255) NOT NULL,
   `correo` varchar(255) NOT NULL,
   `contrasena` varchar(255) NOT NULL,
@@ -268,17 +303,18 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
 --
--- Dumping data for table `usuarios`
+-- Volcado de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`id`, `tipo`, `nombre`, `correo`, `contrasena`, `id_departamento`, `baja`) VALUES
-(1, 1, 'Javier Gómez', 'javier.gomez@emaginarte.com', '$2y$10$2iDisbSnjUv3qWM4fb0v9OC1zCXt6wmdyLp0NKjsjLILSspxPUkzO', 1, NULL);
+(1, 1, 'Javier Gómez', 'javier.gomez@emaginarte.com', '$2y$10$2iDisbSnjUv3qWM4fb0v9OC1zCXt6wmdyLp0NKjsjLILSspxPUkzO', 9, NULL),
+(3, 0, 'Prueba', 'prueba@prueba.com', '$2y$10$Z6J4mS/9qA5CWFaocQnY9uY3XHEg9AAso3QRUzlo54XlcSThzD.JS', 10, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Stand-in structure for view `vista_resumen_areas`
--- (See below for the actual view)
+-- Estructura Stand-in para la vista `vista_resumen_areas`
+-- (Véase abajo para la vista actual)
 --
 CREATE TABLE `vista_resumen_areas` (
 `id_area` int(11)
@@ -293,18 +329,18 @@ CREATE TABLE `vista_resumen_areas` (
 -- --------------------------------------------------------
 
 --
--- Structure for view `vista_resumen_areas`
+-- Estructura para la vista `vista_resumen_areas`
 --
 DROP TABLE IF EXISTS `vista_resumen_areas`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vista_resumen_areas`  AS SELECT `ag`.`id` AS `id_area`, `ag`.`nombre` AS `nombre_area`, `ag`.`id_departamento` AS `id_departamento`, `d`.`nombre` AS `nombre_departamento`, sum(case when `t`.`cantidad` > 0 then `t`.`cantidad` else 0 end) AS `ingresos`, sum(case when `t`.`cantidad` < 0 then abs(`t`.`cantidad`) else 0 end) AS `gastos`, ifnull(sum(`t`.`cantidad`),0) AS `total` FROM ((`areas_gastos` `ag` left join `departamentos` `d` on(`ag`.`id_departamento` = `d`.`id`)) left join `transacciones` `t` on(`ag`.`id` = `t`.`id_area`)) GROUP BY `ag`.`id`, `ag`.`nombre`, `ag`.`id_departamento`, `d`.`nombre` ;
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `areas_gastos`
+-- Indices de la tabla `areas_gastos`
 --
 ALTER TABLE `areas_gastos`
   ADD PRIMARY KEY (`id`),
@@ -312,7 +348,7 @@ ALTER TABLE `areas_gastos`
   ADD KEY `areagasto_departamento_idx` (`id_departamento`);
 
 --
--- Indexes for table `departamentos`
+-- Indices de la tabla `departamentos`
 --
 ALTER TABLE `departamentos`
   ADD PRIMARY KEY (`id`),
@@ -320,14 +356,14 @@ ALTER TABLE `departamentos`
   ADD UNIQUE KEY `nombre` (`nombre`);
 
 --
--- Indexes for table `estado`
+-- Indices de la tabla `estado`
 --
 ALTER TABLE `estado`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id_UNIQUE` (`id`);
 
 --
--- Indexes for table `facturas`
+-- Indices de la tabla `facturas`
 --
 ALTER TABLE `facturas`
   ADD PRIMARY KEY (`id`),
@@ -335,7 +371,7 @@ ALTER TABLE `facturas`
   ADD UNIQUE KEY `identificador_UNIQUE` (`identificador`);
 
 --
--- Indexes for table `incidencias`
+-- Indices de la tabla `incidencias`
 --
 ALTER TABLE `incidencias`
   ADD PRIMARY KEY (`id`),
@@ -343,14 +379,14 @@ ALTER TABLE `incidencias`
   ADD KEY `incidencia_pedido_idx` (`id_pedido`);
 
 --
--- Indexes for table `materiales`
+-- Indices de la tabla `materiales`
 --
 ALTER TABLE `materiales`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id_UNIQUE` (`id`);
 
 --
--- Indexes for table `movimientos`
+-- Indices de la tabla `movimientos`
 --
 ALTER TABLE `movimientos`
   ADD PRIMARY KEY (`id`),
@@ -358,7 +394,7 @@ ALTER TABLE `movimientos`
   ADD KEY `movimientos_material_idx` (`id_item`);
 
 --
--- Indexes for table `pedidos`
+-- Indices de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
   ADD PRIMARY KEY (`id`),
@@ -372,7 +408,7 @@ ALTER TABLE `pedidos`
   ADD KEY `fk_Pedidos_Factura1_idx` (`id_factura`);
 
 --
--- Indexes for table `pedidos_estados`
+-- Indices de la tabla `pedidos_estados`
 --
 ALTER TABLE `pedidos_estados`
   ADD PRIMARY KEY (`id`),
@@ -381,7 +417,7 @@ ALTER TABLE `pedidos_estados`
   ADD KEY `pe_pedido_idx` (`id_pedido`);
 
 --
--- Indexes for table `presupuestos`
+-- Indices de la tabla `presupuestos`
 --
 ALTER TABLE `presupuestos`
   ADD PRIMARY KEY (`id`),
@@ -390,7 +426,7 @@ ALTER TABLE `presupuestos`
   ADD KEY `presupuesto_pedido_idx` (`id_pedido`);
 
 --
--- Indexes for table `proveedores`
+-- Indices de la tabla `proveedores`
 --
 ALTER TABLE `proveedores`
   ADD PRIMARY KEY (`id`),
@@ -399,21 +435,21 @@ ALTER TABLE `proveedores`
   ADD KEY `proveedor_servicio_idx` (`id_servicio`);
 
 --
--- Indexes for table `subconceptos`
+-- Indices de la tabla `subconceptos`
 --
 ALTER TABLE `subconceptos`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id_UNIQUE` (`id`);
 
 --
--- Indexes for table `tipos_servicio`
+-- Indices de la tabla `tipos_servicio`
 --
 ALTER TABLE `tipos_servicio`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id_UNIQUE` (`id`);
 
 --
--- Indexes for table `transacciones`
+-- Indices de la tabla `transacciones`
 --
 ALTER TABLE `transacciones`
   ADD PRIMARY KEY (`id`),
@@ -421,7 +457,7 @@ ALTER TABLE `transacciones`
   ADD KEY `transaccion_area_idx` (`id_area`);
 
 --
--- Indexes for table `usuarios`
+-- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`),
@@ -430,123 +466,123 @@ ALTER TABLE `usuarios`
   ADD KEY `usuario_departamento_idx` (`id_departamento`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `areas_gastos`
+-- AUTO_INCREMENT de la tabla `areas_gastos`
 --
 ALTER TABLE `areas_gastos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT for table `departamentos`
+-- AUTO_INCREMENT de la tabla `departamentos`
 --
 ALTER TABLE `departamentos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT for table `estado`
+-- AUTO_INCREMENT de la tabla `estado`
 --
 ALTER TABLE `estado`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `facturas`
+-- AUTO_INCREMENT de la tabla `facturas`
 --
 ALTER TABLE `facturas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `incidencias`
+-- AUTO_INCREMENT de la tabla `incidencias`
 --
 ALTER TABLE `incidencias`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `materiales`
+-- AUTO_INCREMENT de la tabla `materiales`
 --
 ALTER TABLE `materiales`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `movimientos`
+-- AUTO_INCREMENT de la tabla `movimientos`
 --
 ALTER TABLE `movimientos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `pedidos`
+-- AUTO_INCREMENT de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `pedidos_estados`
+-- AUTO_INCREMENT de la tabla `pedidos_estados`
 --
 ALTER TABLE `pedidos_estados`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `presupuestos`
+-- AUTO_INCREMENT de la tabla `presupuestos`
 --
 ALTER TABLE `presupuestos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `proveedores`
+-- AUTO_INCREMENT de la tabla `proveedores`
 --
 ALTER TABLE `proveedores`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `subconceptos`
+-- AUTO_INCREMENT de la tabla `subconceptos`
 --
 ALTER TABLE `subconceptos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `tipos_servicio`
+-- AUTO_INCREMENT de la tabla `tipos_servicio`
 --
 ALTER TABLE `tipos_servicio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `transacciones`
+-- AUTO_INCREMENT de la tabla `transacciones`
 --
 ALTER TABLE `transacciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Constraints for dumped tables
+-- Restricciones para tablas volcadas
 --
 
 --
--- Constraints for table `areas_gastos`
+-- Filtros para la tabla `areas_gastos`
 --
 ALTER TABLE `areas_gastos`
   ADD CONSTRAINT `areagasto_departamento` FOREIGN KEY (`id_departamento`) REFERENCES `departamentos` (`id`);
 
 --
--- Constraints for table `incidencias`
+-- Filtros para la tabla `incidencias`
 --
 ALTER TABLE `incidencias`
   ADD CONSTRAINT `incidencia_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`referencia`);
 
 --
--- Constraints for table `movimientos`
+-- Filtros para la tabla `movimientos`
 --
 ALTER TABLE `movimientos`
   ADD CONSTRAINT `movimientos_material` FOREIGN KEY (`id_item`) REFERENCES `materiales` (`id`);
 
 --
--- Constraints for table `pedidos`
+-- Filtros para la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
   ADD CONSTRAINT `pedido_areagasto` FOREIGN KEY (`id_area_gasto`) REFERENCES `areas_gastos` (`id`),
@@ -557,32 +593,32 @@ ALTER TABLE `pedidos`
   ADD CONSTRAINT `pedido_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
 
 --
--- Constraints for table `pedidos_estados`
+-- Filtros para la tabla `pedidos_estados`
 --
 ALTER TABLE `pedidos_estados`
   ADD CONSTRAINT `pe_estado` FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id`),
   ADD CONSTRAINT `pe_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id`);
 
 --
--- Constraints for table `presupuestos`
+-- Filtros para la tabla `presupuestos`
 --
 ALTER TABLE `presupuestos`
   ADD CONSTRAINT `presupuesto_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`referencia`);
 
 --
--- Constraints for table `proveedores`
+-- Filtros para la tabla `proveedores`
 --
 ALTER TABLE `proveedores`
   ADD CONSTRAINT `proveedor_servicio` FOREIGN KEY (`id_servicio`) REFERENCES `tipos_servicio` (`id`);
 
 --
--- Constraints for table `transacciones`
+-- Filtros para la tabla `transacciones`
 --
 ALTER TABLE `transacciones`
   ADD CONSTRAINT `transaccion_area` FOREIGN KEY (`id_area`) REFERENCES `areas_gastos` (`id`);
 
 --
--- Constraints for table `usuarios`
+-- Filtros para la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD CONSTRAINT `usuario_departamento` FOREIGN KEY (`id_departamento`) REFERENCES `departamentos` (`id`);
