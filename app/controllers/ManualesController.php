306cead7472b6550db287a2d21aed571f56a2d4e
ander.uraga@gmail.com <ander.uraga@gmail.com>
2025-06-19 10:56:35 +0200

    nueva seccion manuales

---
+++ app/controllers/ManualesController.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ . '/../helpers/auth.php';
+require_once __DIR__ . '/../helpers/formatos.php';
+
+class ManualesController extends Controller
+{
+    public function index()
+    {
+        global $usuario;
+        $this->view("manuales/index", []);
+    }
+}
