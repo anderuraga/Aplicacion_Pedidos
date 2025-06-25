e6ff3c5516bfc4074aa385fcf7b3c4a3859d5d15
ander.uraga@gmail.com <ander.uraga@gmail.com>
2025-06-25 10:36:20 +0200

    enlaces para manuales

--- app/views/manuales/index.php
+++ app/views/manuales/index.php
@@ -13,9 +13,179 @@
     <div class="statbox widget box box-shadow">
         <div class="widget-content widget-content-area">
             <h1 class="mt-2 ms-2">Manuales</h1>
-            
 
-                     
+            <ul class="list-group p-4">
+                <li class="list-group-item"><a href="mailto:admincentral@elorrieta-errekamari.com?Subject=Duda%20sobre%20Hoja%20de%20Pedidos"><svg fill="#000000" width="50px" height="50px"
+                            viewBox="0 0 24 24" id="email" data-name="Flat Line" xmlns="http://www.w3.org/2000/svg"
+                            class="icon flat-line">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path id="secondary"
+                                    d="M20.61,5.23l-8,6.28a1,1,0,0,1-1.24,0l-8-6.28A1,1,0,0,0,3,6V18a1,1,0,0,0,1,1H20a1,1,0,0,0,1-1V6A1,1,0,0,0,20.61,5.23Z"
+                                    style="fill: #2ca9bc; stroke-width: 2;"></path>
+                                <path id="primary"
+                                    d="M20,19H4a1,1,0,0,1-1-1V6A1,1,0,0,1,4,5H20a1,1,0,0,1,1,1V18A1,1,0,0,1,20,19ZM20,5H4a1,1,0,0,0-.62.22l8,6.29a1,1,0,0,0,1.24,0l8-6.29A1,1,0,0,0,20,5Z"
+                                    style="fill: none; stroke: #000000; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;">
+                                </path>
+                            </g>
+                        </svg>Escribe un email a adminstración</a></li>
+                <li class="list-group-item"><a
+                        href="https://docs.google.com/document/d/14gx4Ji4YM-MxQMr6ei3mMwYYZ37fUSNPJGH32cwDNDI/edit?usp=sharing"
+                        target="_blank"><svg width="64px" height="64px" viewBox="0 0 9.2 9.2"
+                            style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+                            version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink"
+                            xmlns:xodm="http://www.corel.com/coreldraw/odm/2003" fill="#000000">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <defs>
+                                    <style type="text/css">
+                                        .fil0 {
+                                            fill: none
+                                        }
+
+                                        .fil2 {
+                                            fill: #2554B0
+                                        }
+
+                                        .fil1 {
+                                            fill: #2554B0;
+                                            fill-rule: nonzero
+                                        }
+
+                                        .fil3 {
+                                            fill: #479DFF;
+                                            fill-rule: nonzero
+                                        }
+                                    </style>
+                                </defs>
+                                <g id="Layer_x0020_1">
+                                    <rect class="fil0" height="9.2" width="9.2"></rect>
+                                    <path class="fil1"
+                                        d="M5.81 6.57l-2.42 0c-0.11,0 -0.21,-0.04 -0.28,-0.12l0 0c-0.07,-0.07 -0.12,-0.17 -0.12,-0.28l0 -3.14c0,-0.11 0.05,-0.21 0.12,-0.28l0 0c0.07,-0.08 0.17,-0.12 0.28,-0.12l1.58 0c0.04,0 0.08,0.02 0.11,0.04l1.08 1.09c0.04,0.03 0.05,0.07 0.05,0.12l0 2.29 0 0c0,0.11 -0.05,0.21 -0.12,0.28l0 0c-0.07,0.08 -0.17,0.12 -0.28,0.12zm-2.42 -0.3l2.42 0c0.02,0 0.05,-0.01 0.07,-0.03 0.02,-0.02 0.03,-0.04 0.03,-0.07l0 0 0 -2.24 -1 -1 -1.52 0c-0.03,0 -0.05,0.01 -0.07,0.03 -0.02,0.01 -0.03,0.04 -0.03,0.07l0 3.14c0,0.03 0.01,0.05 0.03,0.07 0.02,0.02 0.04,0.03 0.07,0.03z">
+                                    </path>
+                                    <polygon class="fil2" points="4.97,2.78 4.97,3.86 6.06,3.86 "></polygon>
+                                    <path class="fil1"
+                                        d="M5.12 3.14l0 0.57 0.58 0 -0.58 -0.57zm-0.3 0.72l0 -1.08c0,-0.04 0.02,-0.08 0.05,-0.11 0.05,-0.06 0.15,-0.06 0.21,0l1.08 1.09c0.03,0.02 0.05,0.06 0.05,0.1 0,0.09 -0.07,0.15 -0.15,0.15l-1.09 0c-0.08,0 -0.15,-0.06 -0.15,-0.15z">
+                                    </path>
+                                    <path class="fil3"
+                                        d="M3.95 4.95c-0.09,0 -0.15,-0.07 -0.15,-0.15 0,-0.08 0.06,-0.15 0.15,-0.15l1.3 0c0.09,0 0.15,0.07 0.15,0.15 0,0.08 -0.06,0.15 -0.15,0.15l-1.3 0z">
+                                    </path>
+                                    <path class="fil3"
+                                        d="M3.95 5.7c-0.09,0 -0.15,-0.06 -0.15,-0.15 0,-0.08 0.06,-0.14 0.15,-0.14l0.74 0c0.09,0 0.15,0.06 0.15,0.14 0,0.09 -0.06,0.15 -0.15,0.15l-0.74 0z">
+                                    </path>
+                                </g>
+                            </g>
+                        </svg>Dudas Frecuentes</a></li>
+                <li class="list-group-item"><a href="#" target="_blank"><svg width="50px" height="50px"
+                            viewBox="0 0 1024 1024" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                            fill="#000000">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path
+                                    d="M759.3 352h84.3c15.5 0 28.1 9.2 28.1 20.5v277.3c0 11.3-12.6 20.5-28.1 20.5h-70.3"
+                                    fill="#8CAAFF"></path>
+                                <path
+                                    d="M843.5 695h-70.2v-49.5h70.2c1.4 0 2.5-0.2 3.3-0.4v-268c-0.9-0.2-2-0.4-3.3-0.4h-84.2v-49.5h84.3c29.6 0 52.8 19.9 52.8 45.3v277.3c0 25.4-23.3 45.2-52.9 45.2z"
+                                    fill="#333333"></path>
+                                <path
+                                    d="M203.5 258h531c13.7 0 24.7 11.1 24.7 24.7v457.8c0 13.7-11.1 24.7-24.7 24.7h-531c-13.7 0-24.7-11.1-24.7-24.7V282.7c-0.1-13.6 11-24.7 24.7-24.7z"
+                                    fill="#FFFFFF"></path>
+                                <path
+                                    d="M734.5 790h-531c-27.3 0-49.5-22.2-49.5-49.5V282.7c0-27.3 22.2-49.5 49.5-49.5h531c27.3 0 49.5 22.2 49.5 49.5v457.8c0 27.3-22.2 49.5-49.5 49.5z m-531-507.3v457.8h531V282.7h-531z"
+                                    fill="#333333"></path>
+                                <path
+                                    d="M595.8 521.2L407 656c-5.6 4-13.3 2.7-17.3-2.9-1.5-2.1-2.3-4.6-2.3-7.2V376.3c0-6.8 5.5-12.4 12.4-12.4 2.6 0 5.1 0.8 7.2 2.3L595.8 501c5.6 4 6.9 11.7 2.9 17.3-0.8 1.1-1.8 2.1-2.9 2.9z"
+                                    fill="#8CAAFF"></path>
+                                <path
+                                    d="M399.9 683c-2.1 0-4.1-0.2-6.2-0.5-9.8-1.6-18.4-7-24.1-15.1-4.5-6.3-6.9-13.8-6.9-21.6V376.3c0-20.5 16.6-37.1 37.1-37.1 7.8 0 15.2 2.4 21.6 6.9l188.8 134.8c8.1 5.8 13.4 14.3 15 24.1 1.6 9.8-0.6 19.6-6.4 27.7-2.4 3.4-5.3 6.3-8.7 8.7L421.3 676.2c-6.3 4.5-13.8 6.8-21.4 6.8z m12.2-282.6v221.5l155.1-110.7-155.1-110.8zM581.4 501s0 0.1 0 0z"
+                                    fill="#333333"></path>
+                            </g>
+                        </svg>Gestionar Inventario</a></li>
+                <li class="list-group-item"><a href="#" target="_blank"><svg width="50px" height="50px"
+                            viewBox="0 0 1024 1024" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                            fill="#000000">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path
+                                    d="M759.3 352h84.3c15.5 0 28.1 9.2 28.1 20.5v277.3c0 11.3-12.6 20.5-28.1 20.5h-70.3"
+                                    fill="#8CAAFF"></path>
+                                <path
+                                    d="M843.5 695h-70.2v-49.5h70.2c1.4 0 2.5-0.2 3.3-0.4v-268c-0.9-0.2-2-0.4-3.3-0.4h-84.2v-49.5h84.3c29.6 0 52.8 19.9 52.8 45.3v277.3c0 25.4-23.3 45.2-52.9 45.2z"
+                                    fill="#333333"></path>
+                                <path
+                                    d="M203.5 258h531c13.7 0 24.7 11.1 24.7 24.7v457.8c0 13.7-11.1 24.7-24.7 24.7h-531c-13.7 0-24.7-11.1-24.7-24.7V282.7c-0.1-13.6 11-24.7 24.7-24.7z"
+                                    fill="#FFFFFF"></path>
+                                <path
+                                    d="M734.5 790h-531c-27.3 0-49.5-22.2-49.5-49.5V282.7c0-27.3 22.2-49.5 49.5-49.5h531c27.3 0 49.5 22.2 49.5 49.5v457.8c0 27.3-22.2 49.5-49.5 49.5z m-531-507.3v457.8h531V282.7h-531z"
+                                    fill="#333333"></path>
+                                <path
+                                    d="M595.8 521.2L407 656c-5.6 4-13.3 2.7-17.3-2.9-1.5-2.1-2.3-4.6-2.3-7.2V376.3c0-6.8 5.5-12.4 12.4-12.4 2.6 0 5.1 0.8 7.2 2.3L595.8 501c5.6 4 6.9 11.7 2.9 17.3-0.8 1.1-1.8 2.1-2.9 2.9z"
+                                    fill="#8CAAFF"></path>
+                                <path
+                                    d="M399.9 683c-2.1 0-4.1-0.2-6.2-0.5-9.8-1.6-18.4-7-24.1-15.1-4.5-6.3-6.9-13.8-6.9-21.6V376.3c0-20.5 16.6-37.1 37.1-37.1 7.8 0 15.2 2.4 21.6 6.9l188.8 134.8c8.1 5.8 13.4 14.3 15 24.1 1.6 9.8-0.6 19.6-6.4 27.7-2.4 3.4-5.3 6.3-8.7 8.7L421.3 676.2c-6.3 4.5-13.8 6.8-21.4 6.8z m12.2-282.6v221.5l155.1-110.7-155.1-110.8zM581.4 501s0 0.1 0 0z"
+                                    fill="#333333"></path>
+                            </g>
+                        </svg>Dar de alta un proveedor nuevo</a></li>
+                <li class="list-group-item"><a href="#" target="_blank"><svg width="50px" height="50px"
+                            viewBox="0 0 1024 1024" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                            fill="#000000">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path
+                                    d="M759.3 352h84.3c15.5 0 28.1 9.2 28.1 20.5v277.3c0 11.3-12.6 20.5-28.1 20.5h-70.3"
+                                    fill="#8CAAFF"></path>
+                                <path
+                                    d="M843.5 695h-70.2v-49.5h70.2c1.4 0 2.5-0.2 3.3-0.4v-268c-0.9-0.2-2-0.4-3.3-0.4h-84.2v-49.5h84.3c29.6 0 52.8 19.9 52.8 45.3v277.3c0 25.4-23.3 45.2-52.9 45.2z"
+                                    fill="#333333"></path>
+                                <path
+                                    d="M203.5 258h531c13.7 0 24.7 11.1 24.7 24.7v457.8c0 13.7-11.1 24.7-24.7 24.7h-531c-13.7 0-24.7-11.1-24.7-24.7V282.7c-0.1-13.6 11-24.7 24.7-24.7z"
+                                    fill="#FFFFFF"></path>
+                                <path
+                                    d="M734.5 790h-531c-27.3 0-49.5-22.2-49.5-49.5V282.7c0-27.3 22.2-49.5 49.5-49.5h531c27.3 0 49.5 22.2 49.5 49.5v457.8c0 27.3-22.2 49.5-49.5 49.5z m-531-507.3v457.8h531V282.7h-531z"
+                                    fill="#333333"></path>
+                                <path
+                                    d="M595.8 521.2L407 656c-5.6 4-13.3 2.7-17.3-2.9-1.5-2.1-2.3-4.6-2.3-7.2V376.3c0-6.8 5.5-12.4 12.4-12.4 2.6 0 5.1 0.8 7.2 2.3L595.8 501c5.6 4 6.9 11.7 2.9 17.3-0.8 1.1-1.8 2.1-2.9 2.9z"
+                                    fill="#8CAAFF"></path>
+                                <path
+                                    d="M399.9 683c-2.1 0-4.1-0.2-6.2-0.5-9.8-1.6-18.4-7-24.1-15.1-4.5-6.3-6.9-13.8-6.9-21.6V376.3c0-20.5 16.6-37.1 37.1-37.1 7.8 0 15.2 2.4 21.6 6.9l188.8 134.8c8.1 5.8 13.4 14.3 15 24.1 1.6 9.8-0.6 19.6-6.4 27.7-2.4 3.4-5.3 6.3-8.7 8.7L421.3 676.2c-6.3 4.5-13.8 6.8-21.4 6.8z m12.2-282.6v221.5l155.1-110.7-155.1-110.8zM581.4 501s0 0.1 0 0z"
+                                    fill="#333333"></path>
+                            </g>
+                        </svg>Crear Hoja de Pedidos < 1.000€</a>
+                </li>
+                <li class="list-group-item"><a href="#" target="_blank"><svg width="50px" height="50px"
+                            viewBox="0 0 1024 1024" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                            fill="#000000">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path
+                                    d="M759.3 352h84.3c15.5 0 28.1 9.2 28.1 20.5v277.3c0 11.3-12.6 20.5-28.1 20.5h-70.3"
+                                    fill="#8CAAFF"></path>
+                                <path
+                                    d="M843.5 695h-70.2v-49.5h70.2c1.4 0 2.5-0.2 3.3-0.4v-268c-0.9-0.2-2-0.4-3.3-0.4h-84.2v-49.5h84.3c29.6 0 52.8 19.9 52.8 45.3v277.3c0 25.4-23.3 45.2-52.9 45.2z"
+                                    fill="#333333"></path>
+                                <path
+                                    d="M203.5 258h531c13.7 0 24.7 11.1 24.7 24.7v457.8c0 13.7-11.1 24.7-24.7 24.7h-531c-13.7 0-24.7-11.1-24.7-24.7V282.7c-0.1-13.6 11-24.7 24.7-24.7z"
+                                    fill="#FFFFFF"></path>
+                                <path
+                                    d="M734.5 790h-531c-27.3 0-49.5-22.2-49.5-49.5V282.7c0-27.3 22.2-49.5 49.5-49.5h531c27.3 0 49.5 22.2 49.5 49.5v457.8c0 27.3-22.2 49.5-49.5 49.5z m-531-507.3v457.8h531V282.7h-531z"
+                                    fill="#333333"></path>
+                                <path
+                                    d="M595.8 521.2L407 656c-5.6 4-13.3 2.7-17.3-2.9-1.5-2.1-2.3-4.6-2.3-7.2V376.3c0-6.8 5.5-12.4 12.4-12.4 2.6 0 5.1 0.8 7.2 2.3L595.8 501c5.6 4 6.9 11.7 2.9 17.3-0.8 1.1-1.8 2.1-2.9 2.9z"
+                                    fill="#8CAAFF"></path>
+                                <path
+                                    d="M399.9 683c-2.1 0-4.1-0.2-6.2-0.5-9.8-1.6-18.4-7-24.1-15.1-4.5-6.3-6.9-13.8-6.9-21.6V376.3c0-20.5 16.6-37.1 37.1-37.1 7.8 0 15.2 2.4 21.6 6.9l188.8 134.8c8.1 5.8 13.4 14.3 15 24.1 1.6 9.8-0.6 19.6-6.4 27.7-2.4 3.4-5.3 6.3-8.7 8.7L421.3 676.2c-6.3 4.5-13.8 6.8-21.4 6.8z m12.2-282.6v221.5l155.1-110.7-155.1-110.8zM581.4 501s0 0.1 0 0z"
+                                    fill="#333333"></path>
+                            </g>
+                        </svg>Crear Hoja de Pedidos > 1.000€</a></li>
+            </ul>
+
 
         </div>
     </div>
